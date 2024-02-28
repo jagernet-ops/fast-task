445c2ce19e6a6269ef5fbb4957e28318108152d6
Michael <mshalewski@outlook.com>
2024-02-28 13:08:56 -0600

    disabled dark mode for initial apk compilation

--- app/src/main/java/com/jager/fasttask/MainActivity.java
+++ app/src/main/java/com/jager/fasttask/MainActivity.java
@@ -1,6 +1,7 @@
 package com.jager.fasttask;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -38,6 +39,7 @@ public class MainActivity extends AppCompatActivity implements OnTaskFragmentClo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    	AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_main);
         getResources = getResources();
         taskDatabaseHelper = TaskListDatabase.getManagementInstance(getApplicationContext());
