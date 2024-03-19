75c117a28df58e363e076077e30bfab431226f76
Michael J. Shalewski <mshalewski@outlook.com>
2024-03-19 01:22:01 -0500

    added documentation

--- app/src/main/java/com/jager/fasttask/MainActivity.java
+++ app/src/main/java/com/jager/fasttask/MainActivity.java
@@ -52,7 +52,6 @@ public class MainActivity extends AppCompatActivity implements OnTaskFragmentClo
 
             }
         });
-        taskDatabaseHelper.getTaskFromFilter(TaskListContract.TaskEntry.COLUMN_NAME_CATEGORY, "Fruit");
         renderedTaskList = new ArrayList<>();
         toDoAdapter = new ToDoAdapter(this, taskDatabaseHelper);
         toDoRecycler.setLayoutManager(new LinearLayoutManager(this));
